97f7914cf242589dd6b1ac28f1adc0a3357b3386
limakilo <simple_random@aliyun.com>
2020-02-06 23:56:05 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="title">
+      <center>app.vue主页面
+        </center>
+    
+        </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+*{
+  margin: 0px;
+  padding: 0px;
+}
+
+.title{
+  background-color: yellowgreen;
+  height: 100px;
+}
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-row class="div">
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+</el-row>
+I
+<svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-renwu"></use>
+</svg>
+
+HANAN
+<svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-renwu2"></use>
+</svg>
+ </div>
+</template>
+<style lang="css">
+  .div{
+    border: 1px solid rebeccapurple
+  }
+  .el-icon-eleme{
+    font-size: 50px
+  }
+  .icon {
+   width: 20em; height: 20em;
+   vertical-align: -0.15em;
+   fill: currentColor;
+   overflow: hidden;
+}
+</style>
